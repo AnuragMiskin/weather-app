c2e713e5956089df45b8b7988dadb05350e02484
ANURAGMISKIN <anuragmiskin@gmail.com>
2024-05-02 16:49:12 +0530

    added a blurred effect to show while data is being fetched

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 import "./style.css";
 import { getWeather } from "./weather";
 
-getWeather(10,10,Intl.DateTimeFormat().resolvedOptions().timeZone).then(res=>{
-    console.log(res.data)
+getWeather(10,10,Intl.DateTimeFormat().resolvedOptions().timeZone).then(renderweather)
+.catch(e=>{
+    console.error(e),
+    alert("Error getting weather")
 })
+
+function renderweather({current,daily,hourly}){
+    // renderCurrentWeather(current)
+    // renderDailyWeather(daily)
+    // renderHourlyWeather(hourly)
+    document.body.classList.remove("blurred")
+}
